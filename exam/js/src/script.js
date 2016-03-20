673368cfdd5266f9cb2bd8ce45194db366c4a4ed
MishaMaksymenko <misha.energy@gmail.com>
2016-03-20 22:19:37 +0200

    cdn masonry

--- exam/js/src/script.js
+++ exam/js/src/script.js
@@ -10,7 +10,7 @@ function getImages(request) {
 	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 	var xhr = new XHR();
 
-	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+request+'&amount=7&size=s', true);
+	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+request+'&amount=7&size=tb', true);
 
 	xhr.onload = function() {
 		var html = $('#images').html();
