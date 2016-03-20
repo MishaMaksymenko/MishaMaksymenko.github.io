2ee7405143db3c4c25243ba35ded82bc90076e97
MishaMaksymenko <misha.energy@gmail.com>
2016-03-20 16:29:27 +0200

    Ch+ Moz+IE9+

--- exam/js/src/script.js
+++ exam/js/src/script.js
@@ -9,7 +9,6 @@ function masonry() {
 function getImages(request) {
 	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 	var xhr = new XHR();
-	alert(XHR);
 
 	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+request+'&amount=7&size=m', true);
 
@@ -17,8 +16,6 @@ function getImages(request) {
 		var html = $('#images').html();
 		var result = xhr.responseText;
 		result = JSON.parse(result)
-		console.log(result);
-		alert(result);
 		var images = result.images;
 		var content = tmpl(html, {data:images});
 		
@@ -27,60 +24,18 @@ function getImages(request) {
 		masonry();
 	}
 
-	xhr.onerror = function() {
-		alert( 'Ошибка ' + this.status );
-	}
-
 	xhr.send();
-
-	// $.ajax({
-	// 	url: 'http://api.pixplorer.co.uk/image?word='+request+'&amount=7&size=m',
-	// 	dataType : "json",
-	// 	success: function (data) {
-	// 		// console.log('data', data.images)
-	// 		var html = $('#images').html();
-	// 		var images = data.images;
-	// 		var content = tmpl(html, {data:images});
-			
-	// 		$('.grid').remove();
-	// 		$('.activities__field').append(content);
-	// 		masonry();
-	// 	}
-	// });
 }
 
 $(function() {
 	$('.flexslider').flexslider({
 		animation: "slide",
-		// controlsContainer: $(".custom-controls-container"),
-  //   	customDirectionNav: $(".custom-navigation a")
 	});
-
-	alert('IE9-');
-
 	getImages();
-	masonry();
 
-	// try {
-	// 	$('#activ-search').on('submit', starter)
-	// } catch (err) {
-	// 	element.attachEvent("onsubmit", starter);
-	// 	// alert('test!IE9');
-	// }
-
-	// // $('#activ-search').on('submit', function(e){
-	// 	// e.preventDefault();
-		
-	// function starter(e) {
-	// 	// console.log('e=',e);
-
-	// 	if (e.preventDefault) {
-	// 		e.preventDefault();
-	// 	} else {
-	// 		e.returnValue = false;
-	// 	}
-	// 	request = $( "input" ).val();
-	// 	getImages(request);
-	// }
-	// });
+	$('#activ-search').on('submit', function(e){
+		e.preventDefault();
+		request = $( "input" ).val();
+		getImages(request);
+	});
 });
