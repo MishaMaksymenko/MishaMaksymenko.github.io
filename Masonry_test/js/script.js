bc47b9882bac9892c7995ad82b3641a70600e6db
MishaMaksymenko <misha.energy@gmail.com>
2016-03-21 13:08:27 +0200

    masonry IE test

---
+++ Masonry_test/js/script.js
@@ -0,0 +1,41 @@
+
+
+function getImages(request) {
+	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+	var xhr = new XHR();
+
+	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+request+'&amount=7&size=tb', true);
+
+	xhr.onload = function() {
+		var html = $('#images').html();
+		var result = xhr.responseText;
+		result = JSON.parse(result);
+		var images = result.images;
+		var content = tmpl(html, {data:images});
+		
+		$('.grid').remove();
+		$('.activities__field').append(content);
+		$('.grid').masonry({
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer',
+		percentPosition: true
+		});
+	}
+
+	xhr.send();
+}
+
+$(function() {
+
+	getImages();
+
+	$('#activ-search').on('submit', function(e){
+		if (e.preventDefault) {
+			e.preventDefault();
+		} else {
+			e.returnValue = false;
+		}
+		request = $( "input" ).val();
+		getImages(request);
+	});
+});
