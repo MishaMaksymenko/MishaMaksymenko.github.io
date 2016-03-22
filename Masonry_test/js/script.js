c98182b7cec8c6a918d96f72cd9731f08e400e08
MishaMaksymenko <misha.energy@gmail.com>
2016-03-22 15:36:21 +0200

    simple

--- Masonry_test/js/script.js
+++ Masonry_test/js/script.js
@@ -1,48 +1,48 @@
 
 
 function getImages(word) {
-	// var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-	// var xhr = new XHR();
+	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+	var xhr = new XHR();
 
-	// xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=tb', true);
+	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=tb', true);
 
-	// xhr.onload = function() {
-	// 	var html = $('#images').html();
-	// 	var result = xhr.responseText;
-	// 	var images_data = JSON.parse(result);
-	// 	var images = images_data.images;
-	// 	var content = tmpl(html, {data:images});
+	xhr.onload = function() {
+		var html = $('#images').html();
+		var result = xhr.responseText;
+		var images_data = JSON.parse(result);
+		var images = images_data.images;
+		var content = tmpl(html, {data:images});
 		
-	// 	$('.grid').remove();
-	// 	$('.activities__field').append(content);
-	// 	$('.grid').masonry({
-	// 		columnWidth: '.grid-sizer',
-	// 		itemSelector: '.grid-item',
-	// 		percentPosition: true,
-	// 	});
-	// }
+		$('.grid').remove();
+		$('.activities__field').append(content);
+		$('.grid').masonry({
+			columnWidth: '.grid-sizer',
+			itemSelector: '.grid-item',
+			percentPosition: true,
+		});
+	}
 
-	// xhr.send();
+	xhr.send();
 
-	 $.ajax({
-		url: 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=tb',
-		cache: false,
-		dataType : "json",
-		success: function (data) {
-			// console.log('data', data.images)
-			var html = $('#images').html();
-			var images = data.images;
-			var content = tmpl(html, {data:images});
+	//  $.ajax({
+	// 	url: 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=tb',
+	// 	cache: false,
+	// 	dataType : "json",
+	// 	success: function (data) {
+	// 		// console.log('data', data.images)
+	// 		var html = $('#images').html();
+	// 		var images = data.images;
+	// 		var content = tmpl(html, {data:images});
 			
-			$('.grid').remove();
-			$('.activities__field').append(content);
-			$('.grid').masonry({
-				columnWidth: '.grid-sizer',
-				itemSelector: '.grid-item',
-				percentPosition: true,
-			});
-		}
-	});
+	// 		$('.grid').remove();
+	// 		$('.activities__field').append(content);
+	// 		$('.grid').masonry({
+	// 			columnWidth: '.grid-sizer',
+	// 			itemSelector: '.grid-item',
+	// 			percentPosition: true,
+	// 		});
+	// 	}
+	// });
 }
 
 $(function() {
