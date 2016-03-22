d2f2c362d431c9b8da14085d2dcc972651724c23
MishaMaksymenko <misha.energy@gmail.com>
2016-03-22 21:54:47 +0200

    simple

--- Masonry_test/js/script.js
+++ Masonry_test/js/script.js
@@ -1,14 +1,8 @@
 
 
 function getImages(word) {
-	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-	var xhr = new XHR();
-
-	xhr.open('GET', 'http://api.pixplorer.co.uk/image?word=estate&amount=7&size=tb', true);
-
-	xhr.onload = function() {
 		var html = $('#images').html();
-		var result = xhr.responseText;
+		var result = {"images":[{"imageurl":"http://t3.gstatic.com/images?q=tbn:ANd9GcSubRXqPAhohZlJW38Rl-1p0P_mRc3vnKZWDV6QXmhf2OZKEuEiOTaH3vk","word":"estate"},{"imageurl":"http://t2.gstatic.com/images?q=tbn:ANd9GcR5o2FzXJUbPdk9XEb5onPp7hsrv-yQi7gJjoAgxG7e0IqZZjOjv-L9I7Y","word":"estate"},{"imageurl":"http://t1.gstatic.com/images?q=tbn:ANd9GcTEA1qw5qPtrwcs8WkfJz6JhvUJcTyWkGM7d9aINb1SmrZU4rxDNLSJXto","word":"estate"},{"imageurl":"http://t1.gstatic.com/images?q=tbn:ANd9GcQqSvmgQASh4wnU9dWOMuR-bPgLS59nt7Tp1QuKOLjZU5YBx9VFCwp7pc2N","word":"estate"},{"imageurl":"http://t3.gstatic.com/images?q=tbn:ANd9GcTUHZWj_PXBfqbV-Lza_O92Nbd_di3CXrSNB33SDs-kkedMkiqeU4ytfyA","word":"estate"},{"imageurl":"http://t0.gstatic.com/images?q=tbn:ANd9GcRiT8eDWrG-tMCAuQelcgF0RGZWnjVMj85SaxIMnsm-XfsOI9D8M05M5V8","word":"estate"},{"imageurl":"http://t1.gstatic.com/images?q=tbn:ANd9GcQVubQevIE5_gz8jc5v0O4FQJT7R0lYYZPSCVYE153wtCcFRHXfe63VPGA","word":"estate"}],"status":"success","count":7};
 		var images_data = JSON.parse(result);
 		var images = images_data.images;
 		var content = tmpl(html, {data:images});
@@ -20,9 +14,15 @@ function getImages(word) {
 			itemSelector: '.grid-item',
 			percentPosition: true,
 		});
-	}
+	// var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+	// var xhr = new XHR();
 
-	xhr.send();
+	// xhr.open('GET', 'http://api.pixplorer.co.uk/image?word=estate&amount=7&size=tb', true);
+
+	// xhr.onload = function() {
+	// }
+
+	// xhr.send();
 
 	//  $.ajax({
 	// 	url: 'http://api.pixplorer.co.uk/image?word='+word+'&amount=7&size=tb',
