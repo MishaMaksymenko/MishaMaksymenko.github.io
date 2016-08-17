0a28a857b748f4b3ab1a1cb4a78d770d7d3a6b27
MishaMaksymenko <misha.energy@gmail.com>
2016-08-17 10:04:06 +0300

    registration form task

---
+++ loveCrafts/js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+	var mail = document.getElementById('mail');
+	var birthday = document.getElementById('birthday');
+	var password = document.getElementById('password');
+	var passwordConfirm = document.getElementById('password-confirm');
+
+	// date of birth future restriction
+	var today = new Date().toISOString().split('T')[0];
+	birthday.setAttribute('max', today);
+
+	// password validation
+	function matchPassword(){
+	  if(password.value != passwordConfirm.value) {
+	    passwordConfirm.setCustomValidity("Passwords Don't Match");
+	    passwordConfirm.style.backgroundColor = "#d60000";
+	  } else {
+	    passwordConfirm.setCustomValidity('');
+	    passwordConfirm.style.backgroundColor = "#008a00";
+	  }
+	}
+
+	password.onchange = matchPassword;
+	passwordConfirm.onkeyup = matchPassword;
+
+	// form submit handling
+	document.querySelector('form').addEventListener("submit", function(event) {
+		event.preventDefault();
+
+		// disabling the register button
+		var subButton = document.getElementById('sub');
+		subButton.setAttribute('disabled', 'disabled');
+		subButton.style.backgroundColor = "#eee";
+		
+		// AJAX form sending
+		var dataString = 'mail=' + mail.value + '&birthday=' + birthday.value + '&password=' + password.value + '&passwordConfirm=' + passwordConfirm.value;
+		$.ajax({
+			type: "POST",
+			url: "action.php",
+			data: dataString,
+			cache: false,
+			success: function() {
+				alert("Welcome " + mail.value);
+			}
+		});
+	})
+		// alert("Welcome " + mail.value);
+});
